f4b244072b4fb11587d7d31daf3c233e502a1331
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-06-08 18:57:38 -0300

    RepositorioRevisado3

--- src/repository/LivroRepository.ts
+++ src/repository/LivroRepository.ts
@@ -5,50 +5,45 @@ export class LivroRepository{
     private LivroLista : Livro[] = []
     private constructor(){}
     
-    static getInstance():LivroRepository{
+    public static getInstance():LivroRepository{
         if(!this.instance){
             this.instance = new LivroRepository()
         }
         return this.instance
     }
-
-    private findIndex(isbn:string):number{
-        const index = this.LivroLista.findIndex(p=>p.isbn == isbn)
-        if(index == -1){
-            throw new Error("ISBN não encontrado")
-        }
-        return index
-    }
-
     InsereLivro(Livro:Livro){
         this.LivroLista.push(Livro)
     }
 
-    ExibeLivroPorISBN(isbn:string){
-        const index = this.findIndex(isbn)
-        this.LivroLista[index]
+    ExibeLivroPorISBN(isbn:string):Livro|undefined{
+        const index = this.LivroLista.findIndex(l=>l.isbn===isbn)
+        if(index === -1){
+            return undefined;
+        }
+        return this.LivroLista[index];
     }
 
-    RemoveLivroPorISBN(isbn:string){
-        const index = this.findIndex(isbn)
-        this.LivroLista.splice(index,1)
+    RemoveLivroPorISBN(isbn:string):boolean{
+        const index = this.LivroLista.findIndex(l=>l.isbn===isbn)
+        if(index == -1){
+            return false;
+        }
+        this.LivroLista.splice(index,1);
+        return true;
     }
 
     ExibeTodosLivros():Livro[]{
         return this.LivroLista
     }
 
-    AtualizaLivro(isbn:string, LivroAtualizado:Livro):Livro{
-        const index = this.findIndex(isbn)
-        let LivroExistente = this.LivroLista[index];
-        LivroExistente.titulo = LivroAtualizado.titulo;
-        LivroExistente.autor = LivroAtualizado.autor;
-        LivroExistente.editora = LivroAtualizado.editora;
-        LivroExistente.edicao = LivroAtualizado.edicao;
-        LivroExistente.isbn = LivroAtualizado.isbn;
-        LivroExistente.categoria_id = LivroAtualizado.categoria_id;
-
-        return LivroExistente;
+    AtualizaLivro(isbn:string, LivroAtualizado:Livro):Livro|undefined{
+        const index = this.LivroLista.findIndex(l=>l.isbn===isbn)
+        if(index == -1){
+            return undefined;
+        }
+        LivroAtualizado.isbn = isbn;
+        this.LivroLista[index] = LivroAtualizado
+        return this.LivroLista[index];
     }
 }
 
--- src/repository/UsuarioRepository.ts
+++ src/repository/UsuarioRepository.ts
@@ -5,49 +5,45 @@ export class UsuarioRepository{
     private UsuarioLista : Usuario[] = []
     private constructor(){}
     
-    static getInstance():UsuarioRepository{
+    public static getInstance():UsuarioRepository{
         if(!this.instance){
             this.instance = new UsuarioRepository()
         }
         return this.instance
     }
-
-    private findIndex(cpf:string):number{
-        const index = this.UsuarioLista.findIndex(p=>p.cpf == cpf)
-        if(index == -1){
-            throw new Error("ID não encontrado")
-        }
-        return index
-    }
-
     InsereUsuario(usuario:Usuario){
         this.UsuarioLista.push(usuario)
     }
 
     ExibeUsuarioPorCPF(cpf:string):Usuario|undefined{
-        const index = this.findIndex(cpf)
+        const index = this.UsuarioLista.findIndex(u=>u.cpf===cpf)
+        if(index == -1){
+            return undefined;
+        }
         return this.UsuarioLista[index]
     }
 
-    RemoveUsuarioPorCPF(cpf:string){
-        const index = this.findIndex(cpf)
+    RemoveUsuarioPorCPF(cpf:string):boolean{
+        const index = this.UsuarioLista.findIndex(u=>u.cpf===cpf)
+        if(index == -1){
+            return false
+        }
         this.UsuarioLista.splice(index,1)
+        return true;
     }
 
     ExibeTodosUsuarios():Usuario[]{
         return this.UsuarioLista
     }
 
-    AtualizaUsuario(cpf:string, UsuarioAtualizado:Usuario):Usuario{
-        const index = this.findIndex(cpf)
-        let UsuarioExistente = this.UsuarioLista[index];
-        UsuarioExistente.nome = UsuarioAtualizado.nome;
-        UsuarioExistente.cpf = UsuarioAtualizado.cpf;
-        UsuarioExistente.ativo = UsuarioAtualizado.ativo;
-        UsuarioExistente.categoria_id = UsuarioAtualizado.categoria_id;
-        UsuarioExistente.curso_id = UsuarioAtualizado.curso_id;
-
-        return UsuarioExistente;
+    AtualizaUsuario(cpf:string, UsuarioAtualizado:Usuario):Usuario|undefined{
+        const index = this.UsuarioLista.findIndex(u=>u.cpf===cpf)
+        if(index == -1){
+            return undefined;
+        }
+        UsuarioAtualizado.cpf = cpf;
+        this.UsuarioLista[index] = UsuarioAtualizado;
+        return this.UsuarioLista[index];
     }
 }
 
